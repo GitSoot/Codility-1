427c64f106093490f0d676850a8bdc27d16123b2
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-02-24 05:45:19 +0300

    Code implementation

---
+++ Challenge2/charcount.js
@@ -0,0 +1,7 @@
+function countChars(str) {
+  let counts = new Map();
+  for (let char of str) {
+    counts.set(char, (counts.get(char) || 0) + 1);
+  }
+  return Array.from(counts, ([char, count]) => [char, count]);
+}
